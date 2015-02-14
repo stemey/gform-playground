93eabe382dbe3fb7d76082b758be259e9793d245
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2015-02-14 06:54:39 +0100

    initial

---
+++ dojoConfig.js
@@ -0,0 +1,12 @@
+/*jshint unused:false*/
+var dojoConfig = {
+    async: true,
+	baseUrl:"../",
+	parseOnLoad:true,
+    packages: [
+ 		{name:'dojo',location:'dojo'},
+		{name:'dijit',location:'dijit'},
+		{name:'dojox',location:'dojox'},
+		{name: 'gform', location: 'gform'}
+    ]
+};
